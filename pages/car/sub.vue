b4cdc0f919c8d133fd7ae1545fc19e1e6cd16576
linjinghui <852530363@qq.com>
2019-06-27 18:34:03 +0800

    fix

--- pages/car/sub.vue
+++ pages/car/sub.vue
@@ -1,8 +1,25 @@
 <template>
 	<view class="content">
-		购物车子页面，添加订单其他信息
-		<hr>
-		{{JSON.stringify(orderInfo)}}}
+		<ol>
+			<dd class="arrow">
+				<div>送达时间</div>
+				<div>暂无可用</div>
+			</dd>
+		</ol>
+		<ol>
+			<dt class="noline">备注</dt>
+			<dd class="noline">
+				<input type="text" placeholder="输入备注信息" value="" />
+			</dd>
+			<dd>
+				<div>商品</div>
+				<div>3件</div>
+			</dd>
+			<dd>
+				<div>xx</div>
+				<div>x1</div>
+			</dd>
+		</ol>
 	</view>
 </template>
 
@@ -61,7 +78,66 @@
 <style lang="scss">
 	@import '@/common/global.scss';
 	.content {
+		padding-top: 1px;
 		height: 100%;
 		background-color: #f4f5f6;
+	}
+	
+	ol {
+		margin-top: 10px;
+		padding: 0 10px;
+		background-color: #fff;
+		
+		> dt, > dd {
+			position: relative;
+			padding: 14px 0;
+		}
+		> dt {
+			padding: 8px 0;
+		}
+		> dt:not(.noline):after, 
+		> dd:not(.noline):not(:last-of-type):after {
+			content: '';
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			border-bottom: solid 1px #e3e3e3;
+		}
+		> dd.arrow {
+			padding-right: 20px;
+		}
+		> dd.arrow:after {
+			content: '';
+			position: absolute;
+			top: 0;
+			right: 4px;
+			bottom: 0;
+			margin: auto;
+			width: 8px;
+			height: 8px;
+			border: solid 1px #a3a3a3;
+			border-left: 0;
+			border-bottom: 0;
+			transform: rotate(45deg);
+		}
+		> dd {
+			display: flex;
+			flex-shrink: 0;
+			
+			> div,
+			> input {
+				flex: 1;
+			}
+			> div:nth-of-type(2) {
+				text-align: right;
+			}
+			> input {
+				padding: 0 10px;
+				height: 34px;
+				border: solid 1px #e3e3e3;
+				border-radius: 4px;
+			}
+		}
 	}
 </style>
