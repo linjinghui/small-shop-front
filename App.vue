3c99830a137caed7475dd291fc86c432d72df75c
linjinghui <852530363@qq.com>
2019-10-28 10:17:05 +0800

    fix: 新增在线升级功能

--- App.vue
+++ App.vue
@@ -1,8 +1,29 @@
 <script>
 export default {
 	onLaunch: function() {
+		const updateManager = uni.getUpdateManager();		
+		updateManager.onCheckForUpdate(function (res) {
+		  // 请求完新版本信息的回调
+		  // console.log('onCheckForUpdate=', res);
+		});
 		
+		updateManager.onUpdateReady(function (res) {
+		  uni.showModal({
+		    title: '更新提示',
+		    content: '新版本已经准备好，是否重启应用？',
+		    success(res) {
+		      if (res.confirm) {
+		        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+		        updateManager.applyUpdate();
+		      }
+		    }
+		  });
 		
+		});
+		
+		updateManager.onUpdateFailed(function (res) {
+		  // 新的版本下载失败
+		});
 	},
 	onShow: function() {
 		console.log('App Show');
